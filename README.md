f4fae59c5ac7b30b1825e26c2fe2af8bd402e97d
Sergio Martin <econsergio@gmail.com>
2018-09-16 20:03:24 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # factorial-java-function
+## How to run
+```sh
+curl -H "Content-Type: text/plain" localhost:8080/factorial -d 5
+```
---
+++ src/main/java/org/smartinrub/factorialjavafunction/Application.java
@@ -0,0 +1,31 @@
+package org.smartinrub.factorialjavafunction;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.context.FunctionScan;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+@FunctionScan
+@SpringBootApplication
+public class Application {
+
+    @Bean
+    public Function<Long, String> factorial() {
+        return value -> Long.toString(getFactorial(value));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    private long getFactorial(long number) {
+        if (number == 1) {
+            return number;
+        }
+        return getFactorial(number - 1) * number;
+    }
+}
